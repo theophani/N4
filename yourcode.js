587619800aefa30b1d51a375917c02ec0fd7775a
Tiffany Conroy <tiffany.conroy@gmail.com>
2011-01-18 21:41:17 +0100

    better output

--- yourcode.js
+++ yourcode.js
@@ -11,13 +11,30 @@ var TestSuite = (function () {
 
   var _assert = (function () {
     
-    var check = function(condition, comment) {
+    var equals = function equals(a, b) {
+      return a === b;
+    };
+    var notEquals = function notEquals(a, b) {
+      return a !== b;
+    };
+    var echo = function(value) {
+      if (typeof value === 'string') {
+        return "'" + value + "'";
+      } else {
+        return value;
+      }
+    };
+    
+    var check = function(a, b, f, comment) {
       var results = d.getElementById('results');
       var p = d.createElement('p');
-      p.innerHTML   = condition ? 'true' : 'false';
-      p.style.color = condition ? 'green' : 'red';
+      p.innerHTML   = 'a : ' + echo(a) + ', ' +
+                      'b : ' + echo(b) + ', ' +
+                      f.name + ' : ' +
+                      (f(a, b) ? 'true' : 'false');
+                      
+      p.style.color = f(a, b) ? 'green' : 'red';
       results.appendChild(p);
-      console.log(p);
       if (comment !== undefined) {
         log('&nbsp;&nbsp;&nbsp;&nbsp;' + 'Note: ' + comment); 
       }
@@ -25,10 +42,10 @@ var TestSuite = (function () {
     
     return {
       equals : function (a, b, comment) {
-        check(a === b, comment);
+        check(a, b, equals, comment);
       },
       notEquals : function (a, b, comment) {
-        check(a !== b, comment);
+        check(a, b, notEquals, comment);
       }
     };
   }());
@@ -36,7 +53,7 @@ var TestSuite = (function () {
   var _module = (function () {
     return {
       test : function (assertion, assert) {
-       log('&nbsp;&nbsp;' + 'Testing: ' + assertion);
+       log('&nbsp;&nbsp;' + 'Testing: ' + assertion + ' : ' + assert);
        assert(_assert);
       },
       before : function (f) {
@@ -48,6 +65,5 @@ var TestSuite = (function () {
   return { module : function (assertion, module) {
     log('Testing: ' + assertion);
     module(_module);
-    console.log(_module);
   }};
 }());
